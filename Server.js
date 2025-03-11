2021ee01872b7ea00ce453dc9c41f840fc865017
masiharid <masihakhanum08@gmail.com>
2025-03-11 12:56:50 +0400

    Add backend code

--- Server.js
+++ Server.js
@@ -1,22 +1,30 @@
 require('./Src/Connection/connection');
 const express = require('express');
-const App = express();
 const cors = require('cors');
+const App = express();
 const User = require('./Src/Routes/userRoute'); // Ensure this path is correct
 const Store = require('./Src/Routes/productRoute');
 const productadd = require('./Src/Routes/adminRoute');
 
-// Middleware;
-App.use(express.json());
-App.use(cors());
+// CORS Configuration
+const corsOptions = {
+  origin: 'http://localhost:4000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
+};
 
-// Route Controller;
+// Middleware
+App.use(cors(corsOptions));
+App.use(express.json());
+
+// Route Controller
 App.use('/', User);
 App.use('/User', User);
 App.use("/Product", Store);
 App.use('/admin', productadd);
 
-// Port Listening;
+// Port Listening
 const PORT = process.env.PORT || 4000;
 App.listen(PORT, () => {
   console.log('Port is Running on ' + PORT);
--- generateHash.js
+++ generateHash.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 
-const password = 'Km123456'; // Replace with the actual password you want to hash
+const password = 'dpto yixl pckk qkts'; // Replace with the actual password you want to hash
 const saltRounds = 10;
 
 bcrypt.hash(password, saltRounds, function(err, hash) {
